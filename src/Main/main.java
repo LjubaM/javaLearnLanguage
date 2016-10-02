4005218c92035beb002ef023c9f633dd583ede98
LjubaM <ljubov.morozova@itcollege.ee>
2016-10-02 20:11:48 +0300

    Update main.java

--- src/Main/main.java
+++ src/Main/main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main (String[] args) {
 		System.out.println("Alustame leele õppimisega.");
 		System.out.println("Autor Ljuba.");
+		System.out.println("Aasta 2016.");
 	}
 }
